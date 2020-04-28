ab008fee93fc160d1b96ed4515ed53b0ff8ab8e2
AnanyaK1 <ananyak6@illinois.edu>
2020-04-28 15:03:19 -0500

    Update to second screen

--- app/src/main/java/com/example/finalproject_1/SecondScreen.java
+++ app/src/main/java/com/example/finalproject_1/SecondScreen.java
@@ -5,8 +5,23 @@ import android.os.Bundle;
 import android.text.InputType;
 import android.widget.Button;
 import android.widget.EditText;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.widget.ImageView;
+
+//import java.awt.Image;
+//import java.awt.image.BufferedImage;
+//import java.io.File;
+//import java.io.IOException;
+//import java.net.URL;
+//
+//import javafx.scene.image.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
 public class SecondScreen extends AppCompatActivity {
     @Override
@@ -21,4 +36,68 @@ public class SecondScreen extends AppCompatActivity {
             finish();
         });
     }
+
+    public String fileName;
+    public int columns;
+    public int rows;
+    public ImageView imageView;
+
+    public void SplitImage(String setFileName, int setRows, int setColumns) {
+        fileName = setFileName;
+        columns = setColumns;
+        rows = setRows;
+
+
+
+        //https://gist.github.com/madan712/3672616
+
+        try {
+
+            //Provide number of rows and column
+            URL url = new URL(fileName);
+            Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+            imageView.setImageBitmap(bmp);
+
+//                File file = new File(fileName);
+//                Image image = new Image(file.toURI().toString());
+//                imageView = new ImageView(image);
+
+            //BufferedImage originalImgage = ImageView;
+
+            //total width and total height of an image
+            int Width = bmp.getWidth();
+            int Height = bmp.getHeight();
+
+            System.out.println("Image Dimension: " + Width + "x" + Height);
+
+            //width and height of each piece
+            int eWidth = Width / columns;
+            int eHeight = Height / rows;
+
+            int x = 0;
+            int y = 0;
+
+            for (int i = 0; i < rows; i++) {
+                y = 0;
+                for (int j = 0; j < columns; j++) {
+                    try {
+                        System.out.println("creating piece: " + i + " " + j);
+
+                        BufferedImage SubImage = bmp.getSubimage(y, x, eWidth, eHeight);
+                        File outputfile = new File(fileName + i + j + ".jpg");
+                        ImageIO.write(SubImage, "jpg", outputfile);
+
+                        y += eWidth;
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                x += eHeight;
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 }
